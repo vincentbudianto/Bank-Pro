5ca5b73a166cd38f68f6f0529c01aa134d0c3656
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-02 02:22:46 +0700

    perf(router): Added cookie checker for routing

--- src/components/Routes/Router.js
+++ src/components/Routes/Router.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import Cookies from 'universal-cookie';
 import Login from '../Login/Login'
 import Title from '../Title/Title'
 
 function Router() {
-	return (
-		<main>
+	const cookie = new Cookies();
+
+	if (!cookie.get("user")) {
+		return (
 			<Switch>
-				<Route exact path='/' render={() => (<Redirect to="/Title"/>)} />
 				<Route exact path='/Title' component={Title} />
-				<Route exact path='/Login' component={Login} />
+				<Route exact path='/*' component={Login} />
 			</Switch>
-		</main>
-	);
+		);
+	} else {
+		return (
+			<main>
+				<Switch>
+					<Route exact path='/' render={() => (<Redirect to="/Title"/>)} />
+					<Route exact path='/Title' component={Title} />
+					<Route exact path='/*' component={Title} />
+				</Switch>
+			</main>
+		);
+	}
 }
 
 export default Router
