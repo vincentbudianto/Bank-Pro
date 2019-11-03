d9d307e305c46a3120e92baead425777a07b9d37
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-03 21:25:35 +0700

    feat(Transfer): Added receiver account validation

--- src/components/Transfer/Transfer.js
+++ src/components/Transfer/Transfer.js
@@ -75,8 +75,6 @@ class Transfer extends Component {
 
 	handleTransfer = async e => {
 		e.preventDefault();
-		let request = require('request');
-		let xml2js = require('xml2js');
 
 		let receiverAccount = e.target.elements.receiver.value;
 		let transferAmount = e.target.elements.amount.value;
@@ -85,29 +83,29 @@ class Transfer extends Component {
 			document.getElementById('message2-2').innerHTML = `Insufficient balance | Please try again`;
 			document.getElementById('modal-failed').style.display = 'block';
 		} else {
-			let xml =
-				`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://services/">
-					<soapenv:Header/>
-					<soapenv:Body>
-						<ser:Transfer>
-							<account>` + this.state.accountNumber + `</account>
-							<amount>` + transferAmount + `</amount>
-							<targetAccount>` + receiverAccount + `</targetAccount>
-							<realAccount>` + validationResult["accountNumber"] + `</realAccount>
-						</ser:Transfer>
-					</soapenv:Body>
-				</soapenv:Envelope>`;
+			let validationRequest = require('request');
+			let xml2js = require('xml2js');
 
-			let options = {
-				url: 'http://localhost:8085/web_service_bank_pro/services/Transfer?wsdl',
+			let validationXML =
+				`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://services/">
+						<soapenv:Header/>
+						<soapenv:Body>
+							<ser:GetAccountNumber>
+								<account>` + receiverAccount + `</account>
+							</ser:GetAccountNumber>
+						</soapenv:Body>
+					</soapenv:Envelope>`;
+
+			let validationOptions = {
+				url: 'http://localhost:8085/web_service_bank_pro/services/GetAccountNumber?wsdl',
 				method: 'POST',
-				body: xml,
+				body: validationXML,
 				headers: {
 					'Content-Type': 'text/xml;charset=utf-8',
 				}
 			};
 
-			let callback = (error, response, body) => {
+			let validationCallback = (error, response, body) => {
 				if (!error && response.statusCode === 200) {
 					let parser = new DOMParser();
 					let xmlResponse = parser.parseFromString(body, "text/xml");
@@ -119,21 +117,76 @@ class Transfer extends Component {
 
 					xml2js.parseString(resultResponse, xmlOptions, (err, res) => {
 						let json = JSON.stringify(res);
-						let result = JSON.parse(json)["return"];
+						let validationResult = JSON.parse(json)["return"];
+						console.log("validation result : ", validationResult);
 
-						if (result === "200") {
-							document.getElementById('message2-1').innerHTML = `Thank you for transfering using Bank Pro`;
-							document.getElementById('modal-success').style.display = 'block';
+						if (validationResult["status"] === "200") {
+							let request = require('request');
+
+							console.log("account : ", this.state.accountNumber);
+							console.log("balance : ", this.state.balance);
+							console.log("amount : ", transferAmount);
+							console.log("targetAccount : ", receiverAccount);
+							console.log("realAccount : ", validationResult["accountNumber"]);
+
+							let xml =
+								`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://services/">
+									<soapenv:Header/>
+									<soapenv:Body>
+										<ser:Transfer>
+											<account>` + this.state.accountNumber + `</account>
+											<amount>` + transferAmount + `</amount>
+											<targetAccount>` + receiverAccount + `</targetAccount>
+											<realAccount>` + validationResult["accountNumber"] + `</realAccount>
+										</ser:Transfer>
+									</soapenv:Body>
+								</soapenv:Envelope>`;
+
+							let options = {
+								url: 'http://localhost:8085/web_service_bank_pro/services/Transfer?wsdl',
+								method: 'POST',
+								body: xml,
+								headers: {
+									'Content-Type': 'text/xml;charset=utf-8',
+								}
+							};
+
+							let callback = (error, response, body) => {
+								if (!error && response.statusCode === 200) {
+									let parser = new DOMParser();
+									let xmlResponse = parser.parseFromString(body, "text/xml");
+									let resultResponse = xmlResponse.getElementsByTagName("return")[0].outerHTML;
+
+									let xmlOptions = {
+										explicitArray: false
+									};
+
+									xml2js.parseString(resultResponse, xmlOptions, (err, res) => {
+										let json = JSON.stringify(res);
+										let result = JSON.parse(json)["return"];
+
+										if (result === "200") {
+											document.getElementById('message2-1').innerHTML = `Thank you for transfering using Bank Pro`;
+											document.getElementById('modal-success').style.display = 'block';
+										} else {
+											document.getElementById('message2-2').innerHTML = `Unknows error | Please try again`;
+											document.getElementById('modal-failed').style.display = 'block';
+										}
+									});
+								};
+							};
+
+							request(options, callback);
 						} else {
-							document.getElementById('message2-2').innerHTML = `Unknows error | Please try again`;
+							document.getElementById('message2-2').innerHTML = `Account doesn't exist | Please try again`;
 							document.getElementById('modal-failed').style.display = 'block';
 						}
 					});
 				};
 			};
 
-			request(options, callback);
-		};
+			validationRequest(validationOptions, validationCallback);
+		}
 	}
 
 	goToTransactionsHistory() {
