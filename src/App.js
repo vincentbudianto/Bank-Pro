57f23e6e4053a02e5f725ee60806a6859b172d54
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-20 14:40:31 +0700

    style(Bank Pro): Formatting files according to eslint format

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component }  from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import Router from './components/Routes/Router'
+import { BrowserRouter } from 'react-router-dom';
+import Router from './components/Routes/Router';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     super(props);
     this.state = {
       foof: ''
-    }
+    };
   }
 
   render() {
--- src/components/Routes/Router.js
+++ src/components/Routes/Router.js
@@ -41,4 +41,4 @@ function Router() {
 	}
 }
 
-export default Router
+export default Router;
